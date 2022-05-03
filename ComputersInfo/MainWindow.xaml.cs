cc9fcd1c78678336d5f272e90f614ea842daa8ca
Lolli Di <290romchik@gmail.com>
2022-05-03 22:31:08 +0300

    выпадающее меню

--- ComputersInfo/MainWindow.xaml.cs
+++ ComputersInfo/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,6 +24,32 @@ namespace ComputersInfo
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            brd.Height = 0;
+        }
+
+        private bool IsToggle;
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation da = new DoubleAnimation();
+            if (!IsToggle)
+            {
+                da.To = 90;
+                da.Duration = TimeSpan.FromSeconds(1);
+                brd.BeginAnimation(Border.HeightProperty, da);
+                IsToggle = true;
+            }
+            else
+            {
+                da.To = 0;
+                da.Duration = TimeSpan.FromSeconds(1);
+                brd.BeginAnimation(Border.HeightProperty, da);
+                IsToggle = false;
+            }
         }
     }
 }
