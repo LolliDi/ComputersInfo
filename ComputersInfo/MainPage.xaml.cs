124f63c9f877a825ba0a5feb7597686cba784bec
Lolli Di <290romchik@gmail.com>
2022-05-10 11:31:05 +0300

    фикс кнопки в фулл инфо, дата в мин инфо

--- ComputersInfo/MainPage.xaml.cs
+++ ComputersInfo/MainPage.xaml.cs
@@ -1,18 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Animation;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Word = Microsoft.Office.Interop.Word;
 
 namespace ComputersInfo
 {
@@ -164,6 +157,11 @@ namespace ComputersInfo
         private void Update_Click(object sender, RoutedEventArgs e)
         {
             Searching();
+        }
+
+        private void PassportSelected_Click(object sender, RoutedEventArgs e) //создание паспорта всех отображаемых компов
+        {
+            
         }
     }
 }
